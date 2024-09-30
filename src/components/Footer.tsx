5f2706e74a9bac46b87fc51d0c8ef6a06feb5616
alafzah123 <email.alafzah@gmail.com>
2024-09-30 20:55:32 +0530

    logo added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,13 +5,13 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-800 p-4">
       <div className="text-center text-white">
-        <p>
+        <p className="max-sm:text-[12px]">
           &copy; {new Date().getFullYear()} MTS GUPS MATTANNUR. All rights
           reserved.
         </p>
-        <p className="mt-3">
+        <p className="mt-3 max-w-[780px] mx-auto max-sm:text-sm">
           Madhusoodanan Thangal Smaraka Govt U P School, MATTANNUR. PO MATTANNUR
-          - 670592, MATTANNUR Sub District.
+          670592, MATTANNUR Sub District.
         </p>
         <p className="mt-3">
           Powered by{" "}
@@ -29,7 +29,7 @@ const Footer: React.FC = () => {
           <strong>
             <a
               href="https://www.linkedin.com/in/muhammed-irshad-81985b287/"
-              className="underline-none"
+              className="underline-none "
               target="_blank"
               rel="noopener noreferrer"
             >
